4e2354cd591aa5d6523e7e40d74912e789b5a053
shrimatkapoor <shrimatkapoor@gmail.com>
2020-04-26 13:34:35 +0100

    Adds some changes with order of execution

--- utils/src/ChopStick.java
+++ utils/src/ChopStick.java
@@ -2,24 +2,10 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ChopStick {
-  private boolean inUse;
   private Lock lock;
 
   public ChopStick() {
-    putDown();
     this.lock = new ReentrantLock();
-  }
-
-  public void pickUp() {
-    this.inUse = true;
-  }
-
-  public void putDown() {
-    this.inUse = false;
-  }
-
-  public boolean isInUse() {
-    return inUse;
   }
 
   public void lock() {
--- utils/src/Philosopher.java
+++ utils/src/Philosopher.java
@@ -2,37 +2,34 @@ public class Philosopher extends Thread {
 
   private ChopStick leftChop;
   private ChopStick rightChop;
-  private boolean hasEaten;
   private String name;
 
   public Philosopher(ChopStick leftChop, ChopStick rightChop, String name) {
     this.leftChop = leftChop;
     this.rightChop = rightChop;
-    this.hasEaten = false;
     this.name = name;
   }
 
   @Override
   public void run() {
-    while (!hasEaten) {
-      if (leftChop.isInUse() || rightChop.isInUse()) {
-        System.out.println(name + " is thinking...");
-        thinker();
-      } else {
-        leftChop.lock();
-        leftChop.pickUp();
-        rightChop.lock();
-        rightChop.pickUp();
-        System.out.println(name + " is eating...");
-        eater();
-        System.out.println(name + " has finished eating!");
-        hasEaten = true;
-        rightChop.putDown();
-        rightChop.unlock();
-        leftChop.putDown();
-        leftChop.unlock();
-      }
+    ChopStick first, second;
+    if (leftChop.hashCode() <= rightChop.hashCode()) {
+      first = leftChop;
+      second = rightChop;
+    } else {
+      first = rightChop;
+      second = leftChop;
+    }
 
+    while (true) {
+      first.lock();
+      System.out.println(name + " is using his left chopstick");
+      second.lock();
+      System.out.println(name + " is using his right chopstick. Eating.");
+      eater();
+      second.unlock();
+      first.unlock();
+      thinker();
     }
   }
 
